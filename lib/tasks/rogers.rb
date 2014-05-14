9fa51839ecc14b3f5ac52f001e58826a591f8cf6
Kerry Mui <kerry.t.mui@gmail.com>
2014-05-14 14:48:21 -0400

    modified Rogers script with some mechanize attributes

--- lib/tasks/rogers.rb
+++ lib/tasks/rogers.rb
@@ -2,4 +2,28 @@ require_relative "scraper"
 
 class Rogers < Scraper
 
+	def initialize 
+		@retailer = Retailer.where(name: "Test")
+		@contract = Contract.find_by_name("Rogers")
+	end
+
+	def run
+		devices = Device.all
+		devices.each do |dev| 
+
+			page = get_agent.get("http://www.rogers.com/web/Rogers.portal", {
+				:_nfpb => "true",
+				:_pageLabel => "search",
+				:Ntt => "{dev.name}+{dev.model}+16gb" 
+				})
+
+			puts page.body
+
+			# promotion_link = page.links_with(:dom_class => "search_result")[0].click()
+			# new_page = page.links_with(:dom_class => "search_result")[0].click()
+			# puts new_page
+		end
+	end
 end
+
+# /web/Rogers.portal?_nfpb=true&_pageLabel=search
