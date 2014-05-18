99ded64f7085579fb41d9879c71fd1b9693fc79d
Thales Ribeiro <thalessr@gmail.com>
2014-05-18 17:27:03 -0400

    Adding the minimum price feature

--- app/models/device.rb
+++ app/models/device.rb
@@ -2,5 +2,10 @@ class Device < ActiveRecord::Base
 	has_many :summaries
 	has_many :retailers, through: :summaries
 
+	def minimum_price
+		price = Summary.where(device_id: self.id, created_at: (Time.now-5.day)..Time.now).minimum(:price)
+		price
+	end
+
 
 end
