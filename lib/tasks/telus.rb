7a3d6176612b6f54db235e151c4ab2466680c69f
Kerry Mui <kerry.t.mui@gmail.com>
2014-05-30 15:50:52 -0400

    completed initalize method, need help selecting the price from the html class

--- lib/tasks/telus.rb
+++ lib/tasks/telus.rb
@@ -3,9 +3,21 @@ require_relative "scraper"
 class Telus < Scraper
 
 	def initialize
+		@retailer = Retailer.where(name: "Telus").take
+		@contract = Contract.find_by_nae("Telus")
 	end
 
 	def run
+		devices = Device.where(active: true)
+		devices.each do |dev|
+
+			metadatas = Metadata.where(retailer_id: @retailer.id, device_id: dev.id)
+
+			metadatas.each do |m|
+				page = get_agent.get(m.detail)
+				array = page.search('.nobr price').children()
+
+		end
 	end
-	
+
 end
